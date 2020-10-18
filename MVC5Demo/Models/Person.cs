816bdb5a8830d198ba1a546fbf5afb5320f1a073
Weien <bob2993714@gmail.com>
2020-10-18 15:19:58 +0800

    替 Person 模型加上必填(Required)與範圍驗證(Range)

--- MVC5Demo/Models/Person.cs
+++ MVC5Demo/Models/Person.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +10,12 @@ namespace MVC5Demo.Models
     public class Person
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "請輸入姓名")]
+        [DisplayName("姓名")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "請輸入年紀")]
+        [Range(18, 99, ErrorMessage = "年紀限制18~99")]
+        [DisplayName("年紀")]
         public int Age { get; set; }
     }
 }
