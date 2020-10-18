1d214fb7608e59156f873a3b8be7d256f5d85614
Weien <bob2993714@gmail.com>
2020-10-18 12:02:14 +0800

    建立 Person 資料與完成兩個 Create 動作方法

--- MVC5Demo/Controllers/TESTController.cs
+++ MVC5Demo/Controllers/TESTController.cs
@@ -9,18 +9,37 @@ namespace MVC5Demo.Controllers
 {
     public class TESTController : Controller
     {
+        static List<Person> data = new List<Person>() {
+            new Person() { Id = 1, Name = "Mark", Age = 18 },
+            new Person() { Id = 2, Name = "Bob", Age = 19 },
+            new Person() { Id = 3, Name = "Jack", Age = 20 },
+            new Person() { Id = 4, Name = "Kobe", Age = 21 }
+        };
+
         // GET: TEST
         public ActionResult Index()
         {
-            var data = new List<Person>()
-            {
-                new Person() { Id = 1, Name = "Mark", Age = 18 },
-                new Person() { Id = 2, Name = "Bob", Age = 19 },
-                new Person() { Id = 3, Name = "Jack", Age = 20 },
-                new Person() { Id = 4, Name = "Kobe", Age = 21 }
-            };
 
             return View(data);
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                data.Add(person);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
         }
     }
 }
